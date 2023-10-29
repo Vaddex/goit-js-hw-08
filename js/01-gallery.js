4652e077661f2b972f3a4c0068a37b1ce62a225e
Vaddex <89461632+Vaddex@users.noreply.github.com>
2023-10-29 23:20:20 +0300

    added gallery content

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,18 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryUl = document.querySelector(".gallery");
 
+const galleryMaker = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+    <li>
+        <a href="${original}">
+            <img src="${preview}" alt="${description}">
+        </a>
+    </li>`;
+  })
+  .join("");
+
+galleryUl.insertAdjacentHTML("afterbegin", galleryMaker);
+    
 console.log(galleryItems);
