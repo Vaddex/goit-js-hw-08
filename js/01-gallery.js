7e5740423ba245fed51fbc042f1fa950885e874d
Vaddex <89461632+Vaddex@users.noreply.github.com>
2023-10-31 00:22:56 +0300

    added the library on page

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,14 +5,31 @@ const galleryUl = document.querySelector(".gallery");
 const galleryMaker = galleryItems
   .map(({ preview, original, description }) => {
     return `
-    <li>
-        <a href="${original}">
-            <img src="${preview}" alt="${description}">
+    <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img src="${preview}" 
+            alt="${description}"
+            class="gallery__image"
+            data-source="large-image.jpg">
         </a>
     </li>`;
   })
   .join("");
 
 galleryUl.insertAdjacentHTML("afterbegin", galleryMaker);
+galleryUl.addEventListener("click", handlerClick);
+
+function handlerClick(event) {
+  event.preventDefault();
+  const target = event.target;
+  if (target.classList.contains(".gallery__image")) {
+    const mainImg = target.dataset.sourse;
+    modal = basicLigthbox.create(
+      `<img scr='${mainImg}' width='800' height='600'>`
+    );
+    modal.show();
+    modalsOpen = true;
+  }
+}
     
 console.log(galleryItems);
