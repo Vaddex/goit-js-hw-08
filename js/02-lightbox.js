dcff20564d2e91f629eb31ec7ef4dfa597469026
Vaddex <89461632+Vaddex@users.noreply.github.com>
2023-11-06 23:58:19 +0200

    added second task content

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+const galleryUl = document.querySelector(".gallery");
+
+// create gallery
+const galleryMaker = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img class="gallery__image" loading="lazy" src="${preview}" alt="${description}" />
+            </a>
+        </li>
+      `;
+  })
+  .join("");
+
+galleryUl.insertAdjacentHTML("afterbegin", galleryMaker);
+
+const interactiveGallery = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionDelay: 250,
+  fadeSpeed: 250,
+  close: false,
+  captionPosition: "bottom",
+  enableKeyboard: true,
+});
 
 console.log(galleryItems);
